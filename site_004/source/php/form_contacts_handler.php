e6d3449cc805a5a71166512a070502b9bbc9ec12
Miguel Ferreira <miguel.ferreira.2003@outlook.com>
2020-10-06 11:14:45 +0100

    upd: add site_004 w/ css bug on other pages

---
+++ site_004/source/php/form_contacts_handler.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Handler</title>
+</head>
+<body>
+    <?php
+
+        print_r($_POST);
+        die();
+
+    ?>
+</body>
+</html>
